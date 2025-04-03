44c81e35dfd82517d4ed58113ab6f51231dc277e
ia1935 <imranhahmed0987@gmail.com>
2025-04-03 00:43:02 -0400

    springboot setup and docker configuration

---
+++ cbrapi/src/main/java/com/example/cbrapi/CbrapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbrapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbrapiApplication.class, args);
+	}
+
+}
---
+++ cbrapi/src/test/java/com/example/cbrapi/CbrapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cbrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbrapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
